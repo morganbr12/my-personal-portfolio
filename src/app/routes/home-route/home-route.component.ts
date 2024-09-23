206f6e11d8ba953f335027916037510cc3034740
fdankwah <fredrick@appsnmobilesolutions.com>
2024-09-23 11:34:13 +0000

    first commit

---
+++ src/app/routes/home-route/home-route.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home-route',
+  standalone: true,
+  imports: [],
+  templateUrl: './home-route.component.html',
+  styleUrl: './home-route.component.css'
+})
+export class HomeRouteComponent {
+
+}
